a89dfd8db23d395fece2b027aad16bbd3e52e70c
Adam Goetz <atgoetz@gmail.com>
2024-07-09 10:57:00 -0700

    Initial dirty solution for sync

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,26 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
-};
+const syncSortedMerge = (logSources, printer) => {
+  const logs = [];
+
+  logSources.forEach((source) => {
+    let logEntry;
+    // fill the log list by popping logs off the source
+    while ((logEntry = source.pop()) !== false) {
+      logs.push(logEntry); 
+    }
+  });
+
+  // sort
+  logs.sort((a, b) => {
+    return a.date - b.date;
+  });
+
+  logs.forEach((log) => printer.print(log));
+  printer.done();
+
+  return console.log("Sync sort complete?");
+}
+
+module.exports = syncSortedMerge;
